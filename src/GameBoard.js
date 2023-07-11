c0ddbb2a505a0ae022af1b1f326f6c77b0a86f0c
Hardeep Singh <hs0774@gmail.com>
2023-07-11 15:29:17 -0400

    gameBoard now adds 5 shipsto player 1 board and stores coords

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,11 +1,17 @@
+import { Ship } from "./ship.js";
+
+const player1Ships = [new Ship(5),new Ship(4), new Ship(3),new Ship(3),new Ship(2)];
+const player2Ships = [new Ship(5),new Ship(4), new Ship(3),new Ship(3),new Ship(2)];
+
 export class GameBoard {
     constructor(){
         this.boardSize=10;
+        this.coordinates= new Set();
         this.player1board=this.createBoard();
         this.player2board=this.createBoard();
     }
     createBoard(){
-        board = [];
+        const board = [];
         for(let i=0;i<this.boardSize;i++){
             const row =[];
             for(let j=0;j<this.boardSize;j++){
@@ -14,5 +20,21 @@ export class GameBoard {
             board.push(row);
         }
         return board;
+    }
+    placeShip(player1Ships){
+        for (const ship of player1Ships){
+            const x=0;
+            const y=player1Ships.indexOf(ship);
+            for(let i=0;i<ship.length;i++){
+                this.AddShipCoords((x+i),y);
+            }
+        }
+    }
+    AddShipCoords(x,y){
+        const coord = `${x},${y}`;
+        this.coordinates.add(coord);
+    }
+    receiveAttack(){
+
     }
 }
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Ship } from "./ship.js";
+import { GameBoard } from "./GameBoard.js";
+
+//create ships for both players
+// const player1Ships = [new Ship(5),new Ship(4), new Ship(3),new Ship(3),new Ship(2)];
+// const player2Ships = [new Ship(5),new Ship(4), new Ship(3),new Ship(3),new Ship(2)];
+
+//let user place player1 ships player2 ships are random
+
